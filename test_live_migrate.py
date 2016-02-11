bdd75eb3d0ba1a2c3611d8171893027d2c80ce6a
Anthony Lee <anthony.mic.lee@hpe.com>
2016-02-10 17:35:09 -0800

    Updating script to use the python-brick-cinderclient-ext library
    Changed the script completely to now use the APIs that will be
    available in the python-brick-cinderclient-ext library.

--- test_live_migrate.py
+++ test_live_migrate.py
@@ -1,124 +1,146 @@
-"""
-test_live_migrate.py
-
-Description:
-    A sample implementation using the novaclient and live migration verification
-    scripts to test a simple live migration scenario. The scenario being tested
-    live migrates an instance with one volume attached between two hosts n number
-    of times.
-
-    ** Important **
-    Make sure the instance is located on the first host before beginning the test.
-
-Usage:
-    # Start the first flask server API.
-    python verify_live_migration.py
-    # Start the second flask server API.
-    python verify_live_migration.py
-    # Run the client, specifying all required arguments and optional arguments as needed.
-    python test_live_migrate.py -ip1 99.99.999.999 -h1 sample_host1 -ip2 99.99.999.999 -h2 sample_host2 -v 50bceb5d-b6ed-4c04-818e-785acfede07c -s 9907efed-6eb4-4cc3-a03d-08d489a8d07a
-"""
-from novaclient import client as nova_client
-import verify_live_migration_client
-
 import argparse
+import logging
 import os
-import pprint
+import paramiko
+import sys
 import time
 
-def test_live_migration(username, password, tenant, auth, flask_url_a, flask_url_b, host_a, host_b, server_id, vol_ids, runs):
+import brick_cinderclient_ext
+from novaclient import client
+
+LOG = logging.getLogger('__name__')
+LOG.propagate = False
+file_handler = logging.FileHandler("output.log")
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+file_handler.setFormatter(formatter)
+LOG.addHandler(file_handler)
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+LOG.addHandler(stdout_handler)
+stdout_handler.setFormatter(formatter)
+LOG.setLevel(logging.DEBUG)
+
+nova_client = None
+
+def run_live_migration(test_env):
     success = 0
     fail = 0
-    print("DOING %s RUNS" % runs)
-    for attempt in range(0, runs):
-        print("\nATTEMPT %s START" % (attempt + 1))
-        dest_host = ""
-        src_flask = ""
 
-        # Toggle for migrating instances back and forth between hosts.
-        if attempt % 2 == 0:
-            dest_host = host_b
-            src_flask = flask_url_a
-        else:
-            dest_host = host_a
-            src_flask = flask_url_b
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        # Collect a list of all the volume luns on the source.
-        vol_id_list = vol_ids.split(',')
-        vol_luns = []
-        for vol_id in vol_id_list:
-            luns = verify_live_migration_client.fetch_volume_luns(src_flask, vol_id)
-            for lun in luns:
-                if lun not in vol_luns:
-                    vol_luns.append(lun)
-        print("Volume luns on source node:")
-        pprint.pprint(vol_luns)
+    export_cmd = "export OS_USERNAME=%s; export OS_PASSWORD=%s; export OS_AUTH_URL=%s; export OS_TENANT_NAME=%s;" % (test_env['username'], test_env['password'], test_env['auth_url'], test_env['tenant'])
 
-        print("live-migrate to %s" % dest_host)
-        _do_live_migrate(username, password, tenant, auth, dest_host, server_id)
-
-        all_luns = verify_live_migration_client.fetch_all_luns(src_flask)
-        print("all luns on source node after live migration:")
-        pprint.pprint(all_luns)
-
-        # Check that all the volume luns on the source are gone.
-        vol_lun_present = False
-        for lun in vol_luns:
-            if lun in all_luns:
-                vol_lun_present = True
-                break
-        if vol_lun_present:
-            print("FAIL")
+    LOG.info("Running live migration between targets (%s runs)" % test_env['runs'])
+    for attempt in range(0, test_env['runs']):
+        LOG.info("START RUN #%s" % str(attempt + 1))
+        # Determine which host is source and destination
+        src, dest = _determine_source_and_dest(test_env['target_a'], test_env['target_b'], attempt)
+        # Do a precheck of volume paths
+        vol_paths_pre_migrate = []
+        for volume in test_env['volume_ids']:
+            try:
+                ssh.connect(hostname=src['host'], username=src['username'], password=src['password'])
+                cmd = export_cmd + " cinder get-volume-paths %s" % volume
+                if test_env['use_multipath']:
+                    cmd += " --multipath"
+                stdin, stdout, stderr = ssh.exec_command(cmd)
+                vol_paths = stdout.read().strip().split()
+                for path in vol_paths:
+                    if path not in vol_paths_pre_migrate:
+                        vol_paths_pre_migrate.append(path)
+            finally:
+                ssh.close()
+        LOG.info("vol_paths_pre_migrate: %s" % vol_paths_pre_migrate)
+        # Do the live migration
+        LOG.info("Doing live migration from %s to %s" % (src['host'], dest['host']))
+        _do_live_migration(test_env['server_id'], dest)
+        # Do a postcheck of volume paths
+        all_vol_paths_post_migrate = []
+        try:
+            ssh.connect(hostname=src['host'], username=src['username'], password=src['password'])
+            stdin, stdout, stderr = ssh.exec_command(export_cmd + " cinder get-all-volume-paths --multipath %s" % test_env['use_multipath'])
+            all_vol_paths_post_migrate = stdout.read().strip().split()
+        finally:
+            ssh.close()
+        LOG.info("all_vol_paths_post_migrate: %s" % all_vol_paths_post_migrate)
+        # Determine if a success or fail occured
+        vol_path_present = _are_vol_paths_present(vol_paths_pre_migrate, all_vol_paths_post_migrate)
+        if vol_path_present:
+            LOG.info("FAIL")
             fail += 1
         else:
-            print("SUCCESS")
+            LOG.info("SUCCESS")
             success += 1
-        print("ATTEMPT %s END" % (attempt + 1))
+        LOG.info("FINISHED RUN #%s" % str(attempt + 1))
 
-    print("\nTotal runs: %s" % runs)
-    print("Total success: %s" % success)
-    print("Total fail: %s" % fail)
+    LOG.info("success: %s" % success)
+    LOG.info("fail: %s" % fail)        
+        
+def _are_vol_paths_present(vol_paths, all_vol_paths):
+    vol_path_present = False
+    for path in vol_paths:
+        if path in all_vol_paths:
+            vol_path_present = True
+            break
+    return vol_path_present
 
-def _do_live_migrate(username, password, tenant, auth, dest_host, server_id):
-    print("Starting Live Migration to %s." % dest_host)
-    nova = nova_client.Client(2, username, password, tenant, auth)
-    nova.servers.live_migrate(server_id, dest_host, False, False)
-    server = nova.servers.get(server_id)
+def _determine_source_and_dest(target_a, target_b, attempt):
+    src = ""
+    dest = ""
+
+    # Toggle src and dest based on even or odd attempt number
+    if attempt % 2 == 0:
+        dest = target_b
+        src = target_a
+    else:
+        dest = target_a
+        src = target_b
+
+    return src, dest
+
+def _do_live_migration(server_id, dest):
+    nova_client.servers.live_migrate(server_id, dest['host'], False, False)
+    server = nova_client.servers.get(server_id)
     while server.status != "ACTIVE":
-        server = nova.servers.get(server_id)
+        server = nova_client.servers.get(server_id)
         time.sleep(0.1)
-
-    print("Live Migration to %s done." % dest_host)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-username", help="username", default=os.environ['OS_USERNAME'])
-    parser.add_argument("-password", help="password", default=os.environ['OS_PASSWORD'])
-    parser.add_argument("-tenant", help="tenant name", default=os.environ['OS_TENANT_NAME'])
-    parser.add_argument("-auth", help="auth url", default=os.environ['OS_AUTH_URL'])
-    parser.add_argument("-root-helper", help="root helper", default="sudo")
-    parser.add_argument("-ip1", help="ip of first flask server", required=True)
-    parser.add_argument("-ip2", help="ip of second flask server", required=True)
-    parser.add_argument("-port1", help="port of first flask server", default=5002, type=int)
-    parser.add_argument("-port2", help="port of second flask server", default=5002, type=int)
-    parser.add_argument("-v", "--volume-ids", help="volume ids, comma separated", required=True)
-    parser.add_argument("-s", "--server-id", help="server id", required=True)
-    parser.add_argument("-r", "--runs", help="number of times to perform live migration", default=2, type=int)
+    parser.add_argument("-u", "--username", help="username", default=os.environ['OS_USERNAME'])
+    parser.add_argument("-p", "--password", help="password", default=os.environ['OS_PASSWORD'])
+    parser.add_argument("-t", "--tenant", help="tenant", default=os.environ['OS_TENANT_NAME'])
+    parser.add_argument("-a", "--auth", help="auth url", default=os.environ['OS_AUTH_URL'])
     parser.add_argument("-h1", "--host1", help="name of the first host", required=True)
+    parser.add_argument("-h1u", "--host1-user", help="user for the first host", required=True)
+    parser.add_argument("-h1p", "--host1-password", help="password for the first host", required=True)
     parser.add_argument("-h2", "--host2", help="name of the second host", required=True)
+    parser.add_argument("-h2u", "--host2-user", help="user for the second host", required=True)
+    parser.add_argument("-h2p", "--host2-password", help="password for the second host", required=True)
+    parser.add_argument("-r", "--runs", help="number of runs to do", type=int, default=2)
+    parser.add_argument("-m", "--multipath", help="use multipath", action="store_true", default=False)
+    parser.add_argument("-s", "--server-id", help="server id", required=True)
+    parser.add_argument("-v", "--volume-ids", help="volume ids, comma separated", required=True)
     args = parser.parse_args()
 
-    flask_url1 = "http://%s:%s/" % (args.ip1, args.port1)
-    flask_url2 = "http://%s:%s/" % (args.ip2, args.port2)
+    nova_client = client.Client(2, args.username, args.password, args.tenant, args.auth)
+    target_a = {"host": args.host1, "username": args.host1_user, "password": args.host1_password}
+    target_b = {"host": args.host2, "username": args.host2_user, "password": args.host2_password}
+    volume_ids = args.volume_ids.split(',')
+    test_env = {
+        "target_a": target_a,
+        "target_b": target_b,
+        "volume_ids": volume_ids,
+        "server_id": args.server_id,
+        "use_multipath": args.multipath,
+        "runs": args.runs,
+        "username": args.username,
+        "password": args.password,
+        "tenant": args.tenant,
+        "auth_url": args.auth,
+    }
 
-    test_live_migration(args.username,
-                        args.password,
-                        args.tenant,
-                        args.auth,
-                        flask_url1,
-                        flask_url2,
-                        args.host1,
-                        args.host2,
-                        args.server_id,
-                        args.volume_ids,
-                        args.runs)
+    run_live_migration(test_env)
